80525ced3271c8580a9dec26d82ab9a3bc779b54
Juan José Pérez Abad <58952176+juanjomalouu@users.noreply.github.com>
2022-02-27 11:37:16 +0100

    t

---
+++ Juez2/Mergesort.py
@@ -0,0 +1,36 @@
+
+def mergesort(A, p, r):
+    if p<r:
+        q = (p+r)//2
+        mergesort(A, p, q)
+        mergesort(A, q+1, r)
+        merge(A,p,q,r)
+    return A
+
+#Merge combina todos los arrays que han sido ordenados
+def merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    L = [0]*(n1+1)
+    R = [0]*(n2+1)
+    for i in range(n1):
+        L[i] = A[p + i]
+    for j in range(n2):
+        R[j] = A[q + 1 + j]
+    L[n1] = float('inf')
+    R[n2] = float('inf')
+    i = 0
+    j = 0
+    for k in range(p,r+1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i+=1
+        else:
+            A[k] = R[j]
+            j+=1
+    return A
+
+N = int(input())
+numData = list(map(int, input().strip().split(" ")))
+mergesort(numData,0,N-2)
+print(numData)
--- Juez2/darkSouls.py
+++ Juez2/darkSouls.py
@@ -1,43 +1,40 @@
+# Busqueda Binaria
 def BusquedaBinaria(A,ini,fin,x):
+    mitad = (ini + fin) // 2
     if ini>fin:
-        return -1
+        return mitad
     else:
-        mitad=(ini+fin)//2
         if x == A[mitad]:
-            return x
+            return mitad
         else:
             if x > A[mitad]:
                 return BusquedaBinaria(A,mitad+1,fin,x)
             else:
                 return BusquedaBinaria(A,ini,mitad-1,x)
 
+# Coger los datos de la entrada
 numEnemigos=int(input())
-
 listaEnemigos = list(map(int, input().strip().split(" ")))
 listaEnemigos.sort()
-
-
 listaNiveles = list()
-
 numNiveles = int(input())
-
 for i in range(0, numNiveles):
     listaNiveles.append(int(input()))
 
-listaRespuesta = list()
-
-for i in listaNiveles:
-    numFinal = listaEnemigos[numEnemigos-1]
-    if(i<numFinal):
-        listaRespuesta.append(BusquedaBinaria(listaEnemigos, 0, numEnemigos-1, i))
+#Sumar todos los valores
+listaSumas = list()
+for i in range(0,numEnemigos):
+    if(i == 0):
+        listaSumas.append(listaEnemigos[i])
     else:
-        listaRespuesta.append(numFinal)
+        listaSumas.append(listaEnemigos[i]+listaSumas[i-1])
 
-
-
-for i in listaRespuesta:
-    print(i,"", end="")
-    suma=0
-    for j in range(0,i):
-        suma+=listaEnemigos[j]
-    print(suma)
+#Buscar que valor nos corresponde
+for i in listaNiveles:
+    if(i>=listaEnemigos[-1]):
+        print(numEnemigos,listaSumas[-1])
+    elif(i<listaEnemigos[0]):
+        print(0,0)
+    else:
+        numTotal = BusquedaBinaria(listaEnemigos, 0, numEnemigos - 1, i)
+        print(numTotal+1,listaSumas[numTotal])
